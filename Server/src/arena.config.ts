8ae223867624334aed1cdc41bee4876bb7a8a580
alubame001 <alubame001@gmail.com>
2022-05-23 20:32:14 +0800

    html

--- Server/src/arena.config.ts
+++ Server/src/arena.config.ts
@@ -111,12 +111,11 @@ export default Arena({
         app.use('/', serveIndex(path.join(__dirname, "static"), {'icons': true}))        
         app.use('/', express.static(path.join(__dirname, "static")));
 
-        app.use('/game', serveIndex(path.join(__dirname, "public"), {'icons': true}))
+       // app.use('/game', serveIndex(path.join(__dirname, "public"), {'icons': true}))
         app.use('/game', express.static(path.join(__dirname, "public")));
 
         app.use('/admin', serveIndex(path.join(__dirname, "public/admin"), {'icons': true}))
         app.use('/admin', express.static(path.join(__dirname, "public/admin")));
-
         /**
          * Bind @colyseus/monitor
          * It is recommended to protect this route with a password.
